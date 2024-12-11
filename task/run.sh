2eda3cb93f115f7cc459ba74ac38b68f192231f5
Christopher Johns <cjohns@scottlogic.com>
2024-12-11 17:12:57 +0000

    Updated after earlier check.

--- task/run.sh
+++ task/run.sh
@@ -21,19 +21,19 @@ if [ -z "$DATASET_JOBS" ]; then
     DATASET_JOBS=8
 fi
 
-echo Getting config
-if [ -n "$COLLECTION_DATASET_BUCKET_NAME_NOT_SET_FOR_TEST" ]; then
-    aws s3 sync s3://$(COLLECTION_DATASET_BUCKET_NAME)/config/$(COLLECTION_DIR)/$(REPOSITORY) $(COLLECTION_DIR) --no-progress
-    aws s3 sync s3://$(COLLECTION_DATASET_BUCKET_NAME)/config/$(PIPELINE_DIR)/$(REPOSITORY) $(PIPELINE_DIR) --no-progress
-else
-    echo "COLECTION_DATASET_BUCKET_NAME not set, unable to get config (it will be obtained from the prod CDN later)"
-fi
-
 echo Update makerules
 make makerules
 
 echo Install dependencies
 make init
+
+echo Getting config
+if [ -n "$COLLECTION_DATASET_BUCKET_NAME" ]; then
+    aws s3 sync s3://$(COLLECTION_DATASET_BUCKET_NAME)/config/$(COLLECTION_DIR)/$(REPOSITORY) $(COLLECTION_DIR) --no-progress
+    aws s3 sync s3://$(COLLECTION_DATASET_BUCKET_NAME)/config/$(PIPELINE_DIR)/$(REPOSITORY) $(PIPELINE_DIR) --no-progress
+else
+    echo "COLECTION_DATASET_BUCKET_NAME not set, unable to get config. (It will be obtained from the produdction CDN later.)"
+fi
 
 echo Run the collector
 make collect
