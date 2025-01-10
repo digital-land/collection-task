74d7e4ceaaa72dc4342ce091a723e6fe0ed156d6
alexglasertpx <alex.glaser@tpximpact.com>
2025-01-10 14:33:30 +0000

    Reverted branch back to 'main'

--- task/makerules/makerules.mk
+++ task/makerules/makerules.mk
@@ -12,7 +12,7 @@ SOURCE_URL=https://raw.githubusercontent.com/digital-land/
 endif
 
 ifeq ($(MAKERULES_URL),)
-MAKERULES_URL=$(SOURCE_URL)makerules/regenerate_log_override/
+MAKERULES_URL=$(SOURCE_URL)makerules/main/
 endif
 
 ifeq ($(CONFIG_URL),)
