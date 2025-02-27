41598a84fb2c1208ced3109d0878694832768575
eveleighoj <35256612+eveleighoj@users.noreply.github.com>
2025-02-27 17:02:46 +0000

    add a second script tthat can be used for just collecting and processing

---
+++ task/run-collect-and-transform.sh
@@ -0,0 +1,52 @@
+#! /bin/bash
+
+set -e
+
+TODAY=$(date +%Y-%m-%d)
+echo "Running collector for $COLLECTION_NAME on $TODAY"
+
+if [ -z "$COLLECTION_DATASET_BUCKET_NAME" ]; then
+    echo "assign value to COLLECTION_DATASET_BUCKET_NAME to save to a bucket" 
+fi
+
+if [ -z "$TRANSFORMED_JOBS" ]; then
+    TRANSFORMED_JOBS=8
+fi
+
+echo Update makerules
+make makerules
+
+echo Install dependencies
+make init
+
+echo Run the collector
+make collect
+
+echo Build the collection database
+make collection
+
+echo Detect new resources that have been downloaded
+make detect-new-resources
+
+if [ -n "$COLLECTION_DATASET_BUCKET_NAME" ]; then
+    echo "Saving logs and resources to $COLLECTION_DATASET_BUCKET_NAME"
+    make save-resources
+    make save-logs
+else
+    echo "No COLLECTION_DATASET_BUCKET_NAME defined so collection files not pushed to s3"
+fi
+
+if [ -n "$COLLECTION_DATASET_BUCKET_NAME" ]; then
+    echo Push collection database to $ENVIRONMENT S3
+    make save-collection
+fi
+
+echo Transform collected files
+gmake transformed -j $TRANSFORMED_JOBS
+
+if [ -n "$COLLECTION_DATASET_BUCKET_NAME" ]; then
+    echo Save transformed files to $ENVIRONMENT S3
+    make save-transformed
+else
+    echo "No COLLECTION_DATASET_BUCKET_NAME defined so transformed files not pushed to s3"
+fi
