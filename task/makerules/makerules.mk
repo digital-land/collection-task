e4f112854cd1417a63b4adb55fae32e14ad418c3
CarlosCoelhoSL <ccoelho@scottlogic.com>
2025-03-05 16:07:41 +0000

    remove branch redirects

--- task/makerules/makerules.mk
+++ task/makerules/makerules.mk
@@ -12,7 +12,7 @@ SOURCE_URL=https://raw.githubusercontent.com/digital-land/
 endif
 
 ifeq ($(MAKERULES_URL),)
-MAKERULES_URL=$(SOURCE_URL)makerules/state-json-updates/
+MAKERULES_URL=$(SOURCE_URL)makerules/main/
 endif
 
 ifeq ($(CONFIG_URL),)
