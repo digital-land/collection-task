5dd83be2cb435587460b00bfa21b5b488a1507ea
CarlosCoelhoSL <ccoelho@scottlogic.com>
2025-03-11 17:24:30 +0000

    branch redirect

--- task/makerules/makerules.mk
+++ task/makerules/makerules.mk
@@ -12,7 +12,7 @@ SOURCE_URL=https://raw.githubusercontent.com/digital-land/
 endif
 
 ifeq ($(MAKERULES_URL),)
-MAKERULES_URL=$(SOURCE_URL)makerules/main/
+MAKERULES_URL=$(SOURCE_URL)makerules/check-state-in-collection-pipeline-makerules/
 endif
 
 ifeq ($(CONFIG_URL),)
